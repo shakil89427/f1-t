9583661cc14a4032c9b3540ef3255914e21e41b3
shakil89427 <shakilahmed89427@gmail.com>
2022-03-27 01:16:38 +0600

    checked

--- index.js
+++ index.js
@@ -6,18 +6,21 @@ const regex = /^[a-zA-Z]+$/;
 
 submitForm.addEventListener("submit", function () {
   event.preventDefault();
-  const password = event.target[0].value;
-  const password2 = event.target[1].value;
-  if (password.length < 8) {
+  const password = event.target[0];
+  const password2 = event.target[1];
+  if (password2.value.length < 8) {
     error.innerText = "Password should be 8 characters long";
+    password2.classList.add("border-[#EB5757]");
     return error.classList.remove("invisible");
   }
-  if (password !== password2) {
+  if (password.value !== password2.value) {
     error.innerText = "Password didn't matched";
+    password2.classList.add("border-[#EB5757]");
     return error.classList.remove("invisible");
   }
-  if (regex.test(password)) {
+  if (regex.test(password.value)) {
     error.innerText = "Password cannot be only alphabets";
+    password2.classList.add("border-[#EB5757]");
     return error.classList.remove("invisible");
   }
   submitForm.classList.add("hidden");
