28985c9fa05033231542731eca2a1ab4278f2078
shakil89427 <shakilahmed89427@gmail.com>
2022-03-27 01:03:45 +0600

    done

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-/* Get user input */
-document.getElementById("submit_form").addEventListener("submit", function () {
+const submitForm = document.getElementById("submit_form");
+const error = document.getElementById("error");
+const success = document.getElementById("success");
+
+const regex = /^[a-zA-Z]+$/;
+
+submitForm.addEventListener("submit", function () {
   event.preventDefault();
-  const error = document.getElementById("error");
   const password = event.target[0].value;
   const password2 = event.target[1].value;
-  console.log(error.classList);
+  if (password.length < 8) {
+    error.innerText = "Password should be 8 characters long";
+    return error.classList.remove("invisible");
+  }
+  if (password !== password2) {
+    error.innerText = "Password didn't matched";
+    return error.classList.remove("invisible");
+  }
+  if (regex.test(password)) {
+    error.innerText = "Password cannot be only alphabets";
+    return error.classList.remove("invisible");
+  }
+  submitForm.classList.add("hidden");
+  success.classList.remove("hidden");
 });
